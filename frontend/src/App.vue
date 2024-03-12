be0656658bc12860c64b13f68c10aff4c094943b
MarcosBruur <marcosbruur@gmail.com>
2024-03-12 16:12:49 -0300

    creando entorno virtual y configurando Django con VueJS

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import {ref,onBeforeMount} from 'vue'
+import config from './components/token.js'
+
+export default{
+  components:{
+
+  },
+  setup(){
+    let token = config.token
+
+    return{
+      token,
+    }
+  }
+  
+}
+
+</script>
+
+<template>
+  
+</template>
+
+<style></style>
+
